c93b9bd698d5d7dfab92f6ff27526afebe0f1c7a
DowlathBashaG <9671419+DowlathBashaG@users.noreply.github.com>
2024-04-29 12:06:05 +0530

    Add files via upload
    init commit

---
+++ ratelimiter/src/test/java/io/dowlath/ratelimiter/RatelimiterExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dowlath.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
